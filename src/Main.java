8432951522bc99cd38b2e302e3044b5691ff92a4
SavioHaistem <savio.dantas.5858@gmail.com>
2023-08-23 07:50:09 -0300

    controle de floats

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+        printProducts();
+        forceDouble();
+    }
+
+    static void printProducts() {
+        String product1 = "Computer";
+        String product2 = "Office desk";
+
+        int age = 30;
+        int code = 2345;
+        char gender = 'F';
+
+        double computerPrice = 2100.0;
+        double deskPrice = 650.50;
+        double measure = 53.234567;
+
+        System.out.println(" ");
+        System.out.printf("%s, which price is R$ %.2f reais %n%s which price is R$ %.2f reais %n",product1, computerPrice, product2, deskPrice);
+        System.out.println(" ");
+        System.out.printf("Record: %d, code %d and gender %s%n",age, code, gender);
+        System.out.println(" ");
+        System.out.printf("Measure with eight decimal places: %f%nRounded (three decimal places): %.3f", measure, measure);
+    }
+
+    static void forceDouble() {
+        /*
+        * Ao fazermos uma expressao matematica com numeros inteiros o java ira
+        * considerar que o resultado da operacao sera inteiro para evitar isso devemos fazer
+        * o seguinte:
+        * */
+
+        int x, y;
+        double result;
+        x = 5;
+        y = 2;
+
+        result = (double) x / y;
+        System.out.println(result);
+    }
+}
