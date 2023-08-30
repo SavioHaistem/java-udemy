f969aa4ca2b5df8800460863a4efbeebafc89ebb
SavioHaistem <savio.dantas.5858@gmail.com>
2023-08-30 16:22:22 -0300

    create product object

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+import products.Product;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Product product = new Product();
+        product.name = scan.nextLine();
+        product.quant = scan.nextInt();
+        product.price = scan.nextDouble();
+
+
+    }
 }
