6131a66734ff0c591d2d69c61963a91e7f4558c4
SavioHaistem <savio.dantas.5858@gmail.com>
2023-12-12 10:19:47 -0300

    create Application logic

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,37 @@
+import entities.Employe;
+import entities.OutSourcedEmployee;
+
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the number of employees: ");
+        int manyemployees = scan.nextInt();
+
+        for (int count = 0; count < manyemployees; count++) {
+            System.out.println("Employee #" + (count + 1) + " data:");
+
+            System.out.print("Outsourced ? ");
+            scan.nextLine();
+            boolean outsourced = scan.nextLine().equals("y");
+            System.out.print("name: ");
+            String name = scan.nextLine();
+            System.out.print("hours: ");
+            Integer hours = scan.nextInt();
+            System.out.print("value per hour :");
+            Double valueperhour = scan.nextDouble();
+
+            if (outsourced) {
+                System.out.print("Additional charge: ");
+                Double additional = scan.nextDouble();
+
+                Employe employee = new OutSourcedEmployee(name,hours,valueperhour,additional);
+            }
+
+            Employe employee = new Employe(name,hours,valueperhour);
+        }
     }
+
 }
--- src/entities/OutSourcedEmployee.java
+++ src/entities/OutSourcedEmployee.java
@@ -2,11 +2,22 @@ package entities;
 
 public final class OutSourcedEmployee extends Employe {
 
+    private Double additional;
+
     public OutSourcedEmployee(String name) {
         super(name);
     }
 
-    public OutSourcedEmployee(String name, Integer hours, Double valuePerHour) {
+    public OutSourcedEmployee(String name, Integer hours, Double valuePerHour, Double additional) {
         super(name, hours, valuePerHour);
+        this.additional = additional;
+    }
+
+    public Double getAdditional() {
+        return additional;
+    }
+
+    public void setAdditional(Double additional) {
+        this.additional = additional;
     }
 }
