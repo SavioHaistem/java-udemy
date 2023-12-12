3cb6edfcd398d8864e055346baf9ecf4db015171
SavioHaistem <savio.dantas.5858@gmail.com>
2023-12-12 09:17:49 -0300

    create employers object

--- src/entities/Employe.java
+++ src/entities/Employe.java
@@ -1,16 +1,37 @@
 package entities;
 
 public class Employe {
-    private String name;
+    private final String name;
     private Integer hours;
     private Double valuePerHour;
 
-    public Employe() {
-
+    public Employe(String name) {
+        this.name = name;
     }
+
     public Employe(String name, Integer hours, Double valuePerHour) {
         this.name = name;
         this.hours = hours;
+        this.valuePerHour = valuePerHour;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public void setHours(Integer hours) {
+        this.hours = hours;
+    }
+
+    public Double getValuePerHour() {
+        return valuePerHour;
+    }
+
+    public void setValuePerHour(Double valuePerHour) {
         this.valuePerHour = valuePerHour;
     }
 }
--- src/entities/OutSourcedEmployee.java
+++ src/entities/OutSourcedEmployee.java
@@ -1,5 +1,12 @@
 package entities;
 
-public class OutSourcedEmployee extends Employe {
+public final class OutSourcedEmployee extends Employe {
 
+    public OutSourcedEmployee(String name) {
+        super(name);
+    }
+
+    public OutSourcedEmployee(String name, Integer hours, Double valuePerHour) {
+        super(name, hours, valuePerHour);
+    }
 }
