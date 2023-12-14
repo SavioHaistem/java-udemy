024552815e9dea282a7264cca3892e8b7357b170
SavioHaistem <savio.dantas.5858@gmail.com>
2023-12-14 15:51:14 -0300

    lesson corrections

--- src/Main.java
+++ src/Main.java
@@ -16,23 +16,24 @@ public class Main {
 
         for (int count = 1; count <= numberOfPayers; count++) {
             System.out.println("Tax payer #" + count + " data:");
-            System.out.print("Individual or company (i/c)? ");
             scan.nextLine();
+            System.out.print("Individual or company (i/c)? ");
             char typedChar = scan.next().charAt(0);
 
             System.out.print("Name: ");
+            scan.nextLine();
             String personName = scan.nextLine();
-            System.out.println("Anual income: ");
+            System.out.print("Anual income: ");
             Double anualIncome = scan.nextDouble();
 
             if (typedChar == 'c') {
-                System.out.println("Number of employees: ");
+                System.out.print("Number of employees: ");
                 int employeesCount = scan.nextInt();
 
                 Person person = new LegalPerson(personName,anualIncome,employeesCount);
                 personList.add(person);
             } else {
-                System.out.println("Health expenditures: ");
+                System.out.print("Health expenditures: ");
                 double healthExpenditures = scan.nextDouble();
 
                 Person person = new PhysicalPerson(personName,anualIncome,healthExpenditures);
@@ -41,11 +42,14 @@ public class Main {
         }
 
         Double totalTaxes = 0.0;
+        System.out.println("\n\n =============================== \n");
         System.out.println("TAXES PAID:");
         for (Person person : personList) {
             totalTaxes += person.calculateTax();
             System.out.println(person);
         }
-        System.out.println("TOTAL TAXES: $ " + totalTaxes);
+        System.out.println("\n =============================== \n");
+        System.out.printf("TOTAL TAXES: $%.2f",totalTaxes);
+        System.out.println("\n =============================== \n\n");
     }
 }
