bce785d0024297a0e39d1db0e89947bf07d860c3
SavioHaistem <savio.dantas.5858@gmail.com>
2024-01-09 15:14:53 -0300

    file path informations

---
+++ src/FilePathInformations/Main.java
@@ -0,0 +1,19 @@
+package FilePathInformations;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a file path: ");
+        String strPath = scan.nextLine();
+
+        File path = new File(strPath);
+
+        System.out.println("getName: " + path.getName());
+        System.out.println("getParent: " + path.getParent());
+        System.out.println("getPath: " + path.getPath());
+    }
+}
