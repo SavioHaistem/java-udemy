ff75c6a65db0e1c92f38eeb2786d57e8ea58c7d5
SavioHaistem <savio.dantas.5858@gmail.com>
2024-01-22 10:38:08 -0300

    implement paypal service

--- src/services/PayPalService.java
+++ src/services/PayPalService.java
@@ -1,4 +1,13 @@
 package services;
 
-public class PayPalService {
+public class PayPalService implements TaxService {
+    @Override
+    public double Tax(double amount) {
+        return (amount / 100) * 2;
+    }
+
+    @Override
+    public double SimpleInterest(double amount) {
+        return (amount / 100) * 1;
+    }
 }
